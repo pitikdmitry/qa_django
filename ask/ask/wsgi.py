351c44eebab5e2554036d7f9fe48000d64c490a2
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-09-23 17:00:37 +0300

    init script

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -14,4 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ask.settings')
 
 application = get_wsgi_application()
-# gunicorn -c ../gunicorn.conf.py ask.wsgi
+# gunicorn --daemon -c gunicorn.conf.py ask.wsgi
