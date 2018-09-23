daf12f27a1bc96ec91584e13a065cbbc2af8b0b5
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-09-23 17:53:33 +0300

    allowed hosts

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1&^b*azai758gbajvn$vv+(plx7bkq%hcgjlojeihe)gmv_udw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
