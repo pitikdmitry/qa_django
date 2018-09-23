8b2b1f094ce29c69626d25bd19bc226d244eacce
Pitik Dmitry <pitik.dmitry@mail.ru>
2018-09-23 16:21:57 +0300

    changed conf

---
+++ ask/gunicorn.conf.py
@@ -0,0 +1,20 @@
+bind = "0.0.0.0:8080"
+workers = 5
+logfile = "/home/nyam/study/qa_django/gunicorn.log"
+loglevel = "info"
+proc_name = "blog"
+mode = "wsgi"
+#pidfile = "/home/proft/projects/blog/gunicorn.pid"
+# gunicorn -c ../gunicorn.conf.py ask.wsgi
+# CONFIG = {
+#   'mode': 'wsgi',
+#   'python': '/usr/bin/python3',
+#   'working_dir': '~/study/qa_django/ask',
+#   'args': (
+#     '--bind=0.0.0.0:8000',
+#     '--workers=2',
+#     '--timeout=15',
+#     '--log-level=debug',
+#     'ask.wsgi:application',
+#   ),
+# }
--- gunicorn.conf.py
+++
@@ -1,7 +0,0 @@
-bind = "0.0.0.0:8080"
-workers = 5
-logfile = "/home/nyam/study/qa_django/gunicorn.log"
-loglevel = "info"
-proc_name = "blog"
-#pidfile = "/home/proft/projects/blog/gunicorn.pid"
-# gunicorn -c ../gunicorn.conf.py ask.wsgi
